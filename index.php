74bb323362f4ca7d5bbc7fa0055444f667f1ba72
Nidan Shrestha <nidanshrestha@gmail.com>
2021-03-15 21:57:05 +0545

    scss file created

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <link src="stylesheets/main.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="melody">
+            <p>damn</p>
+        </div>
+    </body>
+</html>
