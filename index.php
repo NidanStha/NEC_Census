0557cdcbd65b0f87364a1ed70ca9c98ad8a156de
Nidan Shrestha <nidanshrestha@gmail.com>
2021-03-17 19:26:16 +0545

    header and nav

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <html>
     <head>
-        <link src="stylesheets/main.css" rel="stylesheet">
+        <link rel="stylesheet" href="stylesheets/main.css" type="text/css" />
     </head>
     <body>
-        <div class="melody">
-            <p>damn</p>
+      <div class="wrapper">
+        <div class="header">
+          <div class="logo">
+            <a href="#">
+              <img src="Images/logo.png" alt="NEC Logo" />
+              Nepali Evangelical Church
+            </a>
+          </div>
         </div>
+
+        <nav>
+          <ul class="menubar">
+            <a href="#"><li>Members</li></a>
+            <a href="#"><li>Add Members</li></a>
+            <a href="#"><li>Summery</li></a>
+          </ul>
+        </nav>
+      </div>
     </body>
 </html>
